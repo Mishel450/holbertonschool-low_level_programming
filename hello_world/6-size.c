47d5a6a268bcc63737dac8c794e0392d44e57d20
Mishel450 <tomasmishel18@gmail.com>
2023-02-15 10:25:38 -0800

    shell

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,6 +10,6 @@ printf("%lu\n", sizeof(char));
 printf("%lu\n", sizeof(int));
 printf("%lu\n", sizeof(long int));
 printf("%lu\n", sizeof(long long int));
-printf("%lu", sizeof(double));
+printf("%lu", sizeof(float));
 return (0);
 }
