e96676d96cfe0b2dff7481e824a95e003013beee
Mishel450 <tomasmishel18@gmail.com>
2023-02-16 10:23:27 -0800

    c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+int i;
+int z;
+for (i = 0 ; i < 10 ; i++)
+{
+for (z = 97 ; z <= 122 ; z++)
+{
+_putchar(z);
+}
+} 
+_putchar('\n');
+}
