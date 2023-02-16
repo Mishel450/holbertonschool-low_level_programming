7a5cc92de8255c8315758726db0a910fd3a8a21e
Mishel450 <tomasmishel18@gmail.com>
2023-02-16 09:51:51 -0800

    c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -4,8 +4,10 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void);
 {
-    print_alphabet();
-    return (0);
+int i;
+for ( i == 97 ; i >= 122 ; i++)
+_putchar(i);
+return (0);
 }
