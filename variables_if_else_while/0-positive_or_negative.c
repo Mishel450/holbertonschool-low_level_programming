a953f4e3fc010c5763f6eb4462952c29f59accda
Mishel450 <tomasmishel18@gmail.com>
2023-02-16 03:45:21 -0800

    shell

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+init main(void)
+{
+int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	  {
+	    printf("%d is positive");
+	  }
+	if (n = 0)
+	  {
+	    printf("%d is zero");
+	  }   
+	if (n < 0)
+	  {
+	    printf("%d is negative");
+	  }
+	return (0);
+}
