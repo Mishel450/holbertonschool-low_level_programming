00a39385780b36fc4d07f3982a90084da653557a
Mishel450 <tomasmishel18@gmail.com>
2023-02-16 08:18:12 -0800

    shell

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -7,13 +7,13 @@
 int main(void)
 {
 char ch;
-for ( ch = 97 ; ch <= 122 ; ch++)
+for( ch = 97 ; ch <= 122 ; ch++)
 {
-if(ch != 101 && ch != 113)
+if( ch != 101 && ch != 113)
 {
 putchar(ch);
 }
-}  
+}
 putchar('\n');
 return (0);
 }
