6d7f219d9284111343839caf9bb590bd52e69de1
Mishel450 <tomasmishel18@gmail.com>
2023-02-16 05:57:44 -0800

    shell

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char ch;
+ char ch2;
+ for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+  putchar(ch);
+  if (ch == 'z')
+    {
+      for (ch2 = 'A' ; ch <= 'Z' ; ch++)
+	{
+	  putchar(ch2);
+	}
+	}
+ }
+putchar('\n');
+return (0);
+}
