bc316370afc8c7c2a60a30218881d76e4795a492
Mishel450 <tomasmishel18@gmail.com>
2023-02-16 05:05:49 -0800

    shell

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ if (finalNumber > 5)
 {
  printf("Last digit of %d is %d and is greater than 5\n", n, finalNumber);
 }
-if (finalNumber < 6)
+if (finalNumber < 6 && finalNumber != 0)
 {
  printf("Last digit of %d is %d and is less than 6 and not 0\n", n, finalNumber);
 }
