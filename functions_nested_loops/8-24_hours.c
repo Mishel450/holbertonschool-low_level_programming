68df6a1a792b896779cb392d86ed8dc1062b8b09
Mishel450 <tomasmishel18@gmail.com>
2023-02-17 05:19:51 -0800

    c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+int min;
+int hours;
+int i;
+int y;
+ for (y = 0 ; y <= 23 ; y++)
+   {
+     if (y <= 9)
+       {
+	 _putchar(48);
+       }
+     _putchar(y);
+       _putchar(58);
+       for (i = 0 ; y <= 59 ; i++)
+	 {
+	   if (i <= 9)
+	     {
+	       _putchar(48);
+	    }
+	   _putchar(i);
+	 }
+   }
+}
