a02ef3fb0ad4b685fc1534a845ddccf70b315f28
Mishel450 <tomasmishel18@gmail.com>
2023-02-17 04:27:46 -0800

    c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_last_digit - check the code
  * @i: is a character
@@ -7,6 +8,6 @@
 int print_last_digit(int i)
 {
 i = (i % 10);
-printf("%d%d\n" i, i)
+printf("%d%d\n" i, i);
 return (0);
 }
