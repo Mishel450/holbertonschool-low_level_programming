67d46c4c3e0b78ac6980caa10ae7b8ce5de169bf
Mishel450 <tomasmishel18@gmail.com>
2023-02-17 03:56:31 -0800

    c

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,15 +1,17 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * _abs - check the code
  * @r: is a character
- * Return: i absolute valuable
+ * Return: Always 0
  */
 int _abs(int r)
 {
-int i;
-i = abs(r);
-printf("%d\n", i);
-return (0);
+if (r >= 0)
+return (r);
+if (r < -1)
+{
+r = (r * (-1))
+return (r);
+}
 }
