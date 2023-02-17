0da3664c043839441b4f39ac1e0d1fe4bc43e2f2
Mishel450 <tomasmishel18@gmail.com>
2023-02-17 05:24:27 -0800

    c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -8,21 +8,21 @@ void jack_bauer(void)
 {
 int i;
 int y;
- for (y = 0 ; y <= 23 ; y++)
-   {
-     if (y <= 9)
-       {
-	 _putchar(48);
-       }
-     _putchar(y);
-       _putchar(58);
-       for (i = 0 ; y <= 59 ; i++)
-	 {
-	   if (i <= 9)
-	     {
-	       _putchar(48);
-	    }
-	   _putchar(i);
-	 }
-   }
+for (y = 0 ; y <= 23 ; y++)
+{
+if (y <= 9)
+{
+_putchar(48);
+}
+_putchar(y+48);
+_putchar(58);
+for (i = 0 ; y <= 59 ; i++)
+{
+if (i <= 9)
+{
+_putchar(48);
+}
+_putchar(i+48);
+}
+}
 }
