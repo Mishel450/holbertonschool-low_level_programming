25806eb2e5c1181c18887addb7be997282349cf1
Mishel450 <tomasmishel18@gmail.com>
2023-02-17 08:13:34 -0800

    c

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+  int multiply= 48;
+  int number;
+  int result;
+  int i;
+  for (i = 0 ; i <= 9 ; i++)
+    {
+      for (number = 48 ; number <= 57 ; number++)
+	{
+	  result = (number * multiply);
+	    _putchar(result);
+	    _putchar(44);
+	    _putchar(' ');
+	    if(result <= 9)
+	      {
+		_putchar(' ');
+		multiply++;
+	      }
+_putchar('\n');
+	    number++;
+	}
+    }
+}
