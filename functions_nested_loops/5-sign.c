012dcd6de0b1b8ea81f808652341b384d409b1b9
Mishel450 <tomasmishel18@gmail.com>
2023-02-17 03:12:00 -0800

    c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -8,14 +8,14 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-putchar(43);
+_putchar(43);
 return (1);
 }
 if (n < 0)
 {
-putchar(45);
+_putchar(45);
 return (-1);
 }
-purchar(48);
+_purchar(48);
 return (0);
 }
