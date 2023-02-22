5cbb176a99ddfd224681def8b0046a1b979545a0
Mishel450 <tomasmishel18@gmail.com>
2023-02-22 10:07:14 -0800

    loop

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 /**
  * print_square - check the code.
- *
+ * @size: is a chaacter
  * Return: Always 0.
  */
 void print_square(int size)
 {
 int i;
 int j;
-for (j = 1 ; j <= n ; j++)
+for (j = 1 ; j <= size ; j++)
 {
-for (i = 1 ; i <= n ; i++)
+for (i = 1 ; i <= size ; i++)
 {
 _putchar(35);
 }
