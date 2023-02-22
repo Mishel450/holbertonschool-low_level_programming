0b4262117c46b6b702489f99abb4ab2147731e9f
Mishel450 <tomasmishel18@gmail.com>
2023-02-22 08:36:50 -0800

    loop

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - check the code.
+ * @c: is a character.
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 48 ; i <= 57 ; i++)
+{
+_putchar(i);
+}
+_putchar(\n);
+}
