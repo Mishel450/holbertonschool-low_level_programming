3e547b42f523e9d33ddf471336e67e50e004be74
Mishel450 <tomasmishel18@gmail.com>
2023-02-22 10:05:57 -0800

    loop

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_square - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+int i;
+int j;
+for (j = 1 ; j <= n ; j++)
+{
+for (i = 1 ; i <= n ; i++)
+{
+_putchar(35);
+}
+i = 1;
+_putchar('\n');
+}
