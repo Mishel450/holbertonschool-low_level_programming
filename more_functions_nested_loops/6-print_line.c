a6485213533fcf547fafb4bd1bb01257721b5e2c
Mishel450 <tomasmishel18@gmail.com>
2023-02-22 09:21:15 -0800

    loop

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+int i;
+for (i = 0 ; i <= n ; i++)
+{
+_putchar(95);
+}
+_putchar('\n');
+}
