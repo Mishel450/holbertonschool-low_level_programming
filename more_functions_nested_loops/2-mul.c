9b1dbd4319ade09398186054d067dace753dee85
Mishel450 <tomasmishel18@gmail.com>
2023-02-22 08:31:45 -0800

    loop

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - check the code.
+ * @a: is a character.
+ * @b: is a character.
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
