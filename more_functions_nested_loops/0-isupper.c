2547486b9863b6424e65805da5d07c526f88d22f
Mishel450 <tomasmishel18@gmail.com>
2023-02-22 07:16:28 -0800

    loop

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * isupper - check the code.
+ *
+ * Return: Always 0.
+ */
+int isupper(int c);
+{
+if (c = [[:upper:]])
+{
+return (1);
+}
+return (0);
+}
