8a826650cad93425abf9b99b77fc808fa65d90ab
Mishel450 <tomasmishel18@gmail.com>
2023-02-23 05:33:00 -0800

    loop

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -9,11 +9,11 @@ int main(void)
 int i;
 for (i = 1 ; i <= 100 ; i++)
 {
-if ( i % 3 == 0 && i % 5 == 0)
+if (i % 3 == 0 && i % 5 == 0)
 {
 printf("FizzBuzz");
 }
-if ( i % 3 == 0 && i % 5 != 0)
+if (i % 3 == 0 && i % 5 != 0)
 {
 printf("Fizz");
 }
@@ -21,11 +21,14 @@ if (i % 5 == 0 && i % 3 != 0)
 {
 printf("Buzz");
 }
-if ( i % 3 != 0 && i % 5 != 0)
+if (i % 3 != 0 && i % 5 != 0)
 {
 printf("%d", i);
 }
+if (i < 100)
+{
 printf(" ");
+}
 }
 printf("\n");
 return (0);
