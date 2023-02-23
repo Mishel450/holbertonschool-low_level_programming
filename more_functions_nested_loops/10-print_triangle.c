e77b810502ed8e4488da22c78a5d28a6333bc64a
Mishel450 <tomasmishel18@gmail.com>
2023-02-23 03:13:02 -0800

    loop

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_triangle - check the code.
+ * @size: is a chaacter
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+int i;
+int j;
+int z;
+for (j = 1 ; j <= size ; j++)
+{
+for (i = 1 ; i <= size ; i++)
+{
+_putchar(32);
+}
+ for ( z = size ; z >= i  ; z--)
+   {
+     _putchar(35);
+   }
+ i = 1;
+_putchar('\n');
+}
+if (size <= 0)
+{
+_putchar('\n');
+}
+}
