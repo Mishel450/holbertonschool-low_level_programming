83542125d75c1a6af9a0be66a02a85674179fed5
Mishel450 <tomasmishel18@gmail.com>
2023-02-23 05:30:18 -0800

    loop

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,11 +13,11 @@ if ( i % 3 == 0 && i % 5 == 0)
 {
 printf("FizzBuzz");
 }
-if ( i % 3 == 0)
+if ( i % 3 == 0 && i % 5 != 0)
 {
 printf("Fizz");
 }
-if (i % 5 == 0)
+if (i % 5 == 0 && i % 3 != 0)
 {
 printf("Buzz");
 }
