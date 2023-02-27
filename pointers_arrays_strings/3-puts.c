b53092bd2c8ad7901675f5486ba381b2cff2bc49
Mishel450 <tomasmishel18@gmail.com>
2023-02-27 07:51:29 -0800

    98

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,12 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * reset_to_98 - check the code.
+ * _puts - check the code.
  *
  * Return: Always 0.
  */
 void _puts(char *str)
 {
-int puts(char *s);
-return (0);
+int puts(const char *s);
 }
