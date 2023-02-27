41aa71c4a1a3340c9ee14b4e7b7af467e5651755
Mishel450 <tomasmishel18@gmail.com>
2023-02-27 08:31:03 -0800

    98

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * reset_to_98 - check the code.
+ * @n: is a character
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+int i;
+for ( i = s ; i >= 0 ; i--)
+{
+_putchar(s);
+}
+_putchar('\n');
+}
