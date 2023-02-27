d120c01e5f17df325c1d1cde2e864eb1981039ad
Mishel450 <tomasmishel18@gmail.com>
2023-02-27 07:29:06 -0800

    98

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * reset_to_98 - check the code.
+ * @n: is a character
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+return (strlen(s));
+}
