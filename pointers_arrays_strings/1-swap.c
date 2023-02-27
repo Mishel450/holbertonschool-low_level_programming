73f332dc38154bdc73792cae5b165884341a8fda
Mishel450 <tomasmishel18@gmail.com>
2023-02-27 06:21:09 -0800

    98

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * void swap_int - check the code.
+ * @a: is a character.
+ * @b: is a character.
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+*a = b
+*b = a
+}
