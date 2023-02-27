240f29e41d84f8132166cea546c4b4c11d3df9a4
Mishel450 <tomasmishel18@gmail.com>
2023-02-27 08:42:05 -0800

    98

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,15 +1,15 @@
 #include "main.h"
 #include <string.h>
 /**
- * reset_to_98 - check the code.
+ * print_pev - check the code.
  * @s: is a character
  * Return: Always 0.
  */
 void print_rev(char *s)
 {
 int i;
- int size = strlen(s);
-for ( i = size - 1 ; i >= 0 ; i--)
+int size = strlen(s);
+for (i = size - 1 ; i >= 0 ; i--)
 {
 _putchar(s[i]);
 }
