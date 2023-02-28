a948d12944d97bafc16aab6f2c7386ccf4164363
Mishel450 <tomasmishel18@gmail.com>
2023-02-28 06:19:28 -0800

    98

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _atoi - check the code
+ * @s: is a string
+ * Return: n
+ *
+ * ctoi - char to int
+ * Description ctoi: 'char to int'
+ * @c: char to convert.
+ * ctoi return: an int.
+ */
+int ctoi(char c)
+{
+int i = (int)c;
+
+return (i - '0');
+}
+
+int _atoi(char *s)
+{
+unsigned int i, n = 0;
+unsigned int min = 0;  
+unsigned int len = strlen(s);
+
+for (i = 0; i < len; i++)
+{
+if (s[i] == '-')
+{
+min++;
+}
+if (s[i] > 47 && s[i] < 58)
+{
+break;
+}
+}
+while (s[i] > 47 && s[i] < 58)
+{
+n *= 10;
+n += ctoi(s[i]);
+i++
+}
+if (min % 2 == 0)
+{
+return (n);
+}
+else
+{
+return (-n);
+}
+}
