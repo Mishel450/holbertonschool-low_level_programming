cb4d7c241ab010e80af73d555b5119d78bfc7ced
Mishel450 <tomasmishel18@gmail.com>
2023-02-28 04:28:47 -0800

    98

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -10,13 +10,13 @@ void puts_half(char *str)
 int size = strlen(str);
 int sizeD = size / 2;
 int i;
-if (sizeD % 2 != 0)
+if (sizeD % 2 == 0)
 {
 sizeD++;
 }
 for (i = sizeD ; i <= size - 1; i++)
 {
-_putchar(str[i]);    
+_putchar(str[i]);
 }
 _putchar('\n');
 }
