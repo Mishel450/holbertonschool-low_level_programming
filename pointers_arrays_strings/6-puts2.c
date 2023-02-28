0998b3b9bc6f7284093f65921e70449627bc1e99
Mishel450 <tomasmishel18@gmail.com>
2023-02-28 04:04:04 -0800

    98

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -3,14 +3,14 @@
 #include <string.h>
 /**
  * puts2 - check the code.
- *
+ * @str: is a string.
  * Return: Always 0.
  */
 void puts2(char *str)
 {
 int i;
 int size = strlen(str);
-for (i = 0; i <= size -1; i++)
+for (i = 0; i <= size - 1; i++)
 {
 if (i % 2 == 0 && i != 0)
 {
