b9114f2fd3969b102aa79311446f178c747885e5
Mishel450 <tomasmishel18@gmail.com>
2023-02-28 08:58:36 -0800

    98

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -10,11 +10,23 @@
 char *_strncat(char *dest, char *src, int n)
 {
 int i;
-int size = strlen(dest);
+int size1 = strlen(dest);
+int size2 = strlen(src);
+if (n > size2)
+{
+for (i = 0 ; i <= size2 - 1; i++
+{
+dest[size1] = src[i];
+size1++
+}
+}
+else
+{
 for (i = 0 ; i <= n - 1; i++)
 {
-dest[size] = src[i];
-size++;
+dest[size1] = src[i];
+size1++;
+}
 }
 return (dest);
 }
