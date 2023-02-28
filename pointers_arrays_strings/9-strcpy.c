0f5a13d6ad1e81961813ebf988ef962cc5e74697
Mishel450 <tomasmishel18@gmail.com>
2023-02-28 05:43:02 -0800

    98

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcpy - check the code.
+ * @dest: is a pointer
+ * @src: is a string
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest; src);
+}
