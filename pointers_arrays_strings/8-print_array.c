cadbbb5dfc380d0c405346a2169142e05bfc4311
Mishel450 <tomasmishel18@gmail.com>
2023-02-28 05:26:55 -0800

    98

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -8,10 +8,10 @@
  */
 void print_array(int *a, int n)
 {
-int size = strlen(*a);
-for ( ; n <= size ; n++)
+int i;
+for (i = 0 ; i <= n ; i++)
 {
-printf("%d, " a[n]);
+printf("%d, " a[i]);
 }
 printf("\n");
 }
