b1d5cdd8acb44c7811a2e80ec66e0b21a9559cd4
Mishel450 <tomasmishel18@gmail.com>
2023-02-28 05:00:10 -0800

    98

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts_half - check the code.
+ * @a: is a string
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+int size = strlen(a);
+for (i = 0 ; i <= size ; i++)
+{
+printf("%d, " a[n]);
+}
+printf("\n");
+}
