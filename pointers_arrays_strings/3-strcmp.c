187e4a51933ea7a9dc0710aa720e6c7943b70cbe
Mishel450 <tomasmishel18@gmail.com>
2023-03-01 05:18:46 -0800

    98

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -8,22 +8,5 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int i;
-int z1 = 0;
-int z2 = 0;
-int p1 = 0;
-int p2 = 0;
-int size1 = strlen(s1);
-int size2 = strlen(s2);
-for (i = 0 ; i <= size1 ; i++)
-{
-(z1 + s1[i]) = p1;
-z1 = p1;
-}
-for (i = 0 ; i <= size2 ; i++)
-{
-(z2 + s2[i]) = p2;
-z2 = p2;
-}
-return (z1 - z2);
+return (s1 - s2)
 }
