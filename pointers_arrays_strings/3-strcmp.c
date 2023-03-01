ce6ea4954a24c853a8c440cca91a28a82f05e972
Mishel450 <tomasmishel18@gmail.com>
2023-03-01 05:00:24 -0800

    98

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,11 +1,12 @@
 #include "main.h"
 #include <string.h>
 /**
- *
- *
- *
+ * _strcmp - check the code
+ * @s1:
+ * @s2:
+ * Return: Always 0.
  */
 int _strcmp(char *s1, char *s2)
 {
-  return (s1[0] - s2[0])
+return (s1[0] - s2[0]);
 }
