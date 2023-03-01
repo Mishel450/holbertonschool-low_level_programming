b0a5c34a6ef75bb66b25414645bd2127aa39ba70
Mishel450 <tomasmishel18@gmail.com>
2023-03-01 09:16:08 -0800

    98

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -13,16 +13,16 @@ int i;
 int k = 0;
 int j1[30];
 int j2[30];
-for (i = n - 1; i >= 0 ; i--)
+for (i = n; i >= 0 ; i--)
 {
 j2[k] = a[i];
 k++;
 }
-for (i = 1; i <= n; i++)
+for (i = 0; i <= n; i++)
 {
 j1[i] = j2[i];
 }
-for (i = 1; i <= n; i++)
+for (i = 0; i <= n; i++)
 {
 a[i] = j1[i];
 }
