55558867a20b6f754ef268bcd74d44bb4959a64b
Mishel450 <tomasmishel18@gmail.com>
2023-03-01 07:26:24 -0800

    98

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,10 +1,21 @@
 #include "main.h"
 #include <string.h>
+#include <stdio.h>
 /**
- *
- *
- *
+ * reverse_array - check the code.
+ * @a: is a array.
+ * @n: is a number.
+ * Return: nothing.
  */
 void reverse_array(int *a, int n)
 {
+int i;
+for (i = n ; i >= 0 ; i--)
+{
+printf("%d", a);
+if (i != 0)
+{
+printf(", ");
+}
+}
 }
