ce46b66edd95e9e0fc0976c87b35dee6edeeb70e
Mishel450 <tomasmishel18@gmail.com>
2023-03-02 05:58:33 -0800

    98

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,9 @@ char *cap_string(char *s)
 	char z;
 	int j;
 	int size = strlen(s);
-	char SepWords[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	char SepWords[] = {' ', '\t', '\n', ',', ';', '.', '!',
+	       	'?', '"', '(', ')', '{', '}'};
+
 	for (i = 0; i <= size; i++)
 	{
 		for (j = 0; j < 14; j++)
