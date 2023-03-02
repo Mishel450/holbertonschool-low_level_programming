7dd6449ef0b7f5d9f42dc5ae9e5bca17373ac342
Mishel450 <tomasmishel18@gmail.com>
2023-03-02 08:12:14 -0800

    98

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+/**
+ *
+ *
+ *
+ */
+char *leet(char *s)
+{
+	int i;
+	int j;
+	int size = strlen(s);
+	char array[] = { 'a', '4', 'A', '4', 'e', '3' 'E', '3', 'o', '0', 'O', '0', 't', '7', 'T', '7', 'l', '1', 'L', '1'};
+
+	for (i = 0; i <= size; i++)
+	{
+		for (j = 0; j < 20; j++)
+		{
+			if (array[j] == s[i])
+			{
+				*(s + i) = *(array + (j + 1))
+			}
+		}
+	}
+return (s);
+}
