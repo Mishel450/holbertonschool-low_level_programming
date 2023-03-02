3fbd02beb9bfe238b7426afe97218a097906ce21
Mishel450 <tomasmishel18@gmail.com>
2023-03-02 04:36:39 -0800

    98

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -7,23 +7,21 @@
  */
 char *cap_string(char *s)
 {
-int i = 0;
-int j = i;
+int i;
 char upper = 65;
 int lower;
 int size = strlen(s);
-for ( ; i <= size; i++)
+for (i = 0 ; i <= size; i++)
 {
-	if (s[i] == 32 || 9 || 10 || 44 || 59 || 46 || 33 || 63 || 34 || 40 || 41 || 123 || 125)
+	if (s[i + 1] == 32 || 9 || 10 || 44 || 59 || 46 || 33 || 63 || 34 || 40 || 41 || 123 || 125)
 	{
-j++;
-		if (s[j] >= 97 && s[j] <= 122)
+		if (s[i + 1] >= 97 && s[i + 1] <= 122)
 {
 	for (lower = 97 ; lower <= 122; lower++)
 	{
-		if (lower == s[j])
+		if (lower == s[i + 1])
 		{
-s[j] = upper;
+s[i + 1] = upper;
 		break;
 		}
 			upper++;
@@ -32,8 +30,6 @@ s[j] = upper;
 	}	
 lower = 97;
 upper = 65;
-j = i;
-j++;
 }
 return (s);
 }
