81191c00a320b9220721cbeaac4c5280a4593836
Mishel450 <tomasmishel18@gmail.com>
2023-03-02 05:15:11 -0800

    98

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -9,8 +9,6 @@ char *cap_string(char *s)
 {
 int i;
 int j;
-char upper = 65;
-int lower;
 int size = strlen(s);
 char SepWords[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 for (i = 0; i <= size; i++)
@@ -19,13 +17,12 @@ for (i = 0; i <= size; i++)
 			{
 				if (SepWords[j] == s[i])
 				{
-if (s[i + 1] = 97 && s[i + 1] <= 122)
+if (s[i + 1] == 97 && s[i + 1] <= 122)
 		{
 		s[i + 1] - 32;
 		}
 				}
 			}
-:x
 			}
 return (s);
 }
