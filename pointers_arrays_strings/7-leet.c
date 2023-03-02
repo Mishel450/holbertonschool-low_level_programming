939e70390075daec12b57d9244da87d7fda994cc
Mishel450 <tomasmishel18@gmail.com>
2023-03-02 08:49:00 -0800

    98

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -12,7 +12,7 @@ char *leet(char *s)
 	int size = strlen(s);
 	char arrayL[] = { 'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	char arrayN[] = { '4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
-	
+
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < 10; j++)
