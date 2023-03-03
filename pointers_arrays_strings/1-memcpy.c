8db5da62a9db9b1d9ec2feffb35176bd3ec94cf8
Mishel450 <tomasmishel18@gmail.com>
2023-03-03 03:59:11 -0800

    98

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_memscpy - check the code.
+ * @dest: is a pointer.
+ * @src: is a pointer.
+ * @n: is a number.
+ * Return: dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+	
+	for (i = 0; i <= n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+return (dest);
+}
