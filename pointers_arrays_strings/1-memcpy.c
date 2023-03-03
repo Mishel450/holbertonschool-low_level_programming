acd44457e14a632105ea571b7eca8bafa2438ee0
Mishel450 <tomasmishel18@gmail.com>
2023-03-03 04:12:59 -0800

    98

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <string.h>
 /**
- * *_memscpy - check the code.
+ * *_memcpy - check the code.
  * @dest: is a pointer.
  * @src: is a pointer.
  * @n: is a number.
@@ -10,7 +10,7 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		*(dest + i) = *(src + i);
