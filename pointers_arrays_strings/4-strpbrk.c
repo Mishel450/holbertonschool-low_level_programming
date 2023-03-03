651e3bf09959218bb624664eecc325a3196b5c7a
Mishel450 <tomasmishel18@gmail.com>
2023-03-03 05:54:29 -0800

    98

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strpbrk - check the code.
+ * @s: is a pointer.
+ * @accept: is a pointer.
+ * Return: k. 
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char i;
+	char j;
+	char *k = NULL;
+	char sizeS = strlen(s);
+	char sizeAccept = strlen(accept);
+
+	for (i = 0; i <= sizeS; i++)
+	{
+		for (j = 0; j <= sizeAccept; j++)
+		{
+			if (*(accept + j) == *(s + i))
+			{
+				*k = s[i];
+				break;
+			}	
+		}
+	}
+	return (k);
+}
