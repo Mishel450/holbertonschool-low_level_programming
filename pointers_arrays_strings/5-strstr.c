7377c856a6d3a423e43dc8eef28ff7d5ee684906
Mishel450 <tomasmishel18@gmail.com>
2023-03-03 08:32:47 -0800

    98

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,26 +1,27 @@
 #include "main.h"
 #include <string.h>
 /**
- *
- *
- *
- *
+ * *_strstr - check the code.
+ * @haystack: is a pointer.
+ * @needle: is a pointer.
+ * Return: k.
  */
 char *_strstr(char *haystack, char *needle)
 {
 	int i;
-	int j = 0;
-	char *k;
+	int j;
+	char *k = NULL;
 	int v = 0;
 	int size1 = strlen(haystack);
 	int size2 = strlen(needle);
 
 	for (i = 0; i < size1; i++)
 	{
-		b = i
+		b = i;
+j = 0;
 			if (haystack[i] == needle[j] && v == 0)
 			{
-				for ( ; j < size2; j++)
+				for (j = 0 ; j < size2; j++)
 				{
 					if (haystack[b] != needle[j])
 					{
@@ -29,6 +30,7 @@ char *_strstr(char *haystack, char *needle)
 					if (j == size2)
 					{
 						k = &haystack[i];
+					v = 1;
 					}
 					b++;
 				}
