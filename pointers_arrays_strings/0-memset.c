cd4c0a880ff20d2d01b9024b4d5ccd13c51ec8aa
Mishel450 <tomasmishel18@gmail.com>
2023-03-03 03:53:21 -0800

    98

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,34 +1,19 @@
 #include "main.h"
 #include <string.h>
-#include <stdio.h>
 /**
  * *_memset - check the code.
  * @s: is a pointer.
  * @b: is a character.
  * @n: is a number.
- * Return:
+ * Return: s.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-char j = 0;
 
-for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
-		if (i % 10)
-		{
-			*(s + i) = ' ';
-		}
-
-		if (!(i % 10) && i)
-		{
-			*(s + i) = '\n';
-		}
-		if (j == b)
-		{
-			*(s + i) = b;
-		}
-	j++;
+		*(s + i) = b;
 	}
 return (s);
 }
