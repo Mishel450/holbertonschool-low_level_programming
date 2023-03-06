1d2e400e629bddebb15028e8eb5035d839ad2052
Mishel450 <tomasmishel18@gmail.com>
2023-03-06 03:48:47 -0800

    recursion

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,18 +1,16 @@
 #include "main.h"
 /**
- *
- *
- *
+ * _print_rev_recursion - check the code.
+ * @s: is a string.
+ * Return: noting.
  */
 void _print_rev_recursion(char *s)
 {
-	int size = strlen(s);
-int i = size - 1;
-
-	_putchar(s[i]);
-	i--;
-	if (i == 0)
-	{
-		_putchar(10);
-	}
+if (*s == '\0')
+{
+	_putchar(10);
+	return;
+}
+_print_rev_recursion(s + 1);
+_putchar(*s);
 }
