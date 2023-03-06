3f7c04de54e26f690810f94035d4559ef6a1a504
Mishel450 <tomasmishel18@gmail.com>
2023-03-06 05:50:57 -0800

    recursion

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,12 +1,15 @@
 #include "main.h"
+void main()
+{
+int i = 0;
+}
 /**
  * _strlen_recursion - check the code.
  * @s: is a string.
  * Return: nothing.
  */
 int _strlen_recursion(char *s)
-{
-int i; 
+{ 
 if (*s != '\0')
 {
 i++;
