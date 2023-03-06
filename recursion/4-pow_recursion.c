66bfeb7c33c3b0d26fa71fdabbd1d3459eb59154
Mishel450 <tomasmishel18@gmail.com>
2023-03-06 09:24:41 -0800

    recursion

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -6,24 +6,18 @@
  */
 int _pow_recursion(int x, int y)
 {
-	int i = 0;
-	
+
 	if (y < 0)
 	{
 		return (-1);
 	}
 	if (y == 0)
-{
-	return (1);
-}
+	{
+		return (1);
+	}
 	if (y > 0)
 	{
-		i++;
-		return (x * _pow_recursion(x, y));
-	}
-	if (i == y)
-	{
-		return (0);
+		return (x * _pow_recursion(x, y - 1t));
 	}	
 	return (0);
 }
