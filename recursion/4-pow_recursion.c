e84f1c7aadd7486d203b2d236ea27302b5c2ea1f
Mishel450 <tomasmishel18@gmail.com>
2023-03-06 09:27:04 -0800

    recursion

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- *
- *
- *
+ * _pow_recursion - check the code.
+ * @x: is a number.
+ * @y: is a number.
+ * Return: 0.
  */
 int _pow_recursion(int x, int y)
 {
@@ -18,6 +19,6 @@ int _pow_recursion(int x, int y)
 	if (y > 0)
 	{
 		return (x * _pow_recursion(x, y - 1));
-	}	
+	}
 	return (0);
 }
