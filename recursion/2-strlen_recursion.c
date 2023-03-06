93b12c5b9988f18e0ad13a0c3c37f05ff6bbcaf3
Mishel450 <tomasmishel18@gmail.com>
2023-03-06 05:56:28 -0800

    recursion

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -5,10 +5,11 @@
  * Return: nothing.
  */
 int _strlen_recursion(char *s)
-{ 
-	if (*s != '\0')
 {
-return (1 + _strlen_recursion(s + 1));
-}
-return (0);
+
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
 }
