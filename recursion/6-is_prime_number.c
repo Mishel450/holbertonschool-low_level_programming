4d956adf0e2f29a0d120f0eac053e7e6d70e1dc1
Mishel450 <tomasmishel18@gmail.com>
2023-03-07 05:23:29 -0800

    recursion

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+int mainOne(int j, int i, int k)
+{
+ if (i * k == j)
+	 return (0);
+ if (i < j)
+	 return (mainOne(j, (i + 1), k));
+ if (i == j)
+	 return (mainOne(j, 1, (k + 1)));
+ if (k == j)
+	 return (1);
+}
+/**
+ *
+ *
+ *
+ */
+int is_prime_number(int n)
+{
+	return (mainOne(n, 1, 1));
+}
