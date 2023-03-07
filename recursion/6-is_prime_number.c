708f1ded14503fcacacae9d36baef6d389a3cce6
Mishel450 <tomasmishel18@gmail.com>
2023-03-07 05:37:42 -0800

    recursion

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,19 +1,26 @@
 #include "main.h"
+/**
+ * mainOne - check the code.
+ * @j: is (or not) the prime number.  
+ * @i: is a number used to multiply.
+ * @k: is a number used to multiply.
+ * Return: 0 or 1.
+ */
 int mainOne(int j, int i, int k)
 {
- if (i * k == j && k != j)
-	 return (1);
- if (i < j)
-	 return (mainOne(j, (i + 1), k));
- if (i == j && k != j)
-	 return (mainOne(j, 1, (k + 1)));
- else
-	 return (0);
+if (i * k == j && k != j)
+	return (1);
+if (i < j)
+	return (mainOne(j, (i + 1), k));
+if (i == j && k != j)
+	return (mainOne(j, 1, (k + 1)));
+else
+	return (0);
 }
 /**
- *
- *
- *
+ * is_prime_number - check the code.
+ * @n: is (or not) the prime number.
+ * Return: mainOne.
  */
 int is_prime_number(int n)
 {
