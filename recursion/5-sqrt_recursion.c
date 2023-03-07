343e4d2883fdae0c67d0f0223bac412a0829e846
Mishel450 <tomasmishel18@gmail.com>
2023-03-07 03:46:40 -0800

    recursion

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main."
+/**
+ *
+ *
+ *
+ */
+int main(int i)
+{
+
+	if (i * i == n)
+	{
+		return (i);
+	}
+	if (i == j)
+	{
+		return (-1);
+	}
+	if (i < n)
+	{
+		return (main(i + 1));
+	}
+}
+/**
+ *
+ *
+ *
+ */
+int _sqrt_recursion(int n)
+{
+	return (main(i));
+}
