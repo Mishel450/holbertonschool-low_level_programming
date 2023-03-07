485c7d6eda0f3ddf9b118fadb6f3c2608e21670b
Mishel450 <tomasmishel18@gmail.com>
2023-03-07 05:28:57 -0800

    recursion

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -2,13 +2,13 @@
 int mainOne(int j, int i, int k)
 {
  if (i * k == j && k != j)
-	 return (0);
+	 return (1);
  if (i < j)
 	 return (mainOne(j, (i + 1), k));
  if (i == j && k != j)
 	 return (mainOne(j, 1, (k + 1)));
  else
-	 return (1);
+	 return (0);
 }
 /**
  *
