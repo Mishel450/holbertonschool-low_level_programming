aa2a4d054d73609d7821e937d9a007e5197d25c0
Mishel450 <tomasmishel18@gmail.com>
2023-03-07 04:02:28 -0800

    recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,31 +1,34 @@
 #include "main.h"
 /**
- *
- *
- *
+ * main - check the code.
+ * @j: is a number.
+ * @i: is a number.
+ * Return: a number or -1.
  */
-int main(int i)
+int main(int j, int i)
 {
+	int k;
 
-	if (i * i == n)
+	k = i * i;
+	if (k == j)
 	{
 		return (i);
 	}
-	if (i == j)
+	else if (k < j)
+	{
+		return (main(j,(i + 1)));
+	}
+	else
 	{
 		return (-1);
 	}
-	if (i < n)
-	{
-		return (main(i + 1));
-	}
 }
 /**
- *
- *
- *
+ * _sqrt_recursion - check the code.
+ * @n: is the number.
+ * Return: main.
  */
 int _sqrt_recursion(int n)
 {
-	return (main(i));
+	return (main(n, 1));
 }
