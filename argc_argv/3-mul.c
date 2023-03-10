8b4a72d65b945bbf0f315b188654c41aa6da5fcb
Mishel450 <tomasmishel18@gmail.com>
2023-03-10 13:39:47 -0800

    argc argv

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code.
+ * @argc: .
+ * @argv: .
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	int n, m, r;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		n = atoi(argv[1]);
+		m = atoi(argv[2]);
+		r = (m * n);
+		printf("%d\n", r);
+		return (0);
+	}
+}
