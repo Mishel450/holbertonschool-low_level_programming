0d26b2d6c4be155fc286d12a6bcb3de251a3c5d0
Mishel450 <tomasmishel18@gmail.com>
2023-03-10 13:30:19 -0800

    argc argv

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 /**
- *
- *
- *
- *
+ * main - check the code.
+ * @argc: .
+ * @argv: .
+ * Reeturn: 0.
  */
 int main(int argc, char *argv[])
 {
 int i;
 
-for (i = 0; i <= argc - 1)
+for (i = 0; i <= argc - 1; i++)
 (
 printf("d%", argv[i]);
 )
-ret
+return (0);
 }
