4973491fe8a20610898224e0973c11e265ff7cc5
Mishel450 <tomasmishel18@gmail.com>
2023-03-13 09:19:32 -0700

    malloc free

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -6,12 +6,14 @@
  */
 char *create_array(unsigned int size, char c)
 {
-char *j = malloc(sizeof(size));
+	char h;
+	char *j = malloc(sizeof(size));
 	unsigned int i;
 
 	for (i = 0; i >= size; i++)
 	{
-*(j + i) = *(c + i);
+		*(j + i) = *(c + i);
+		h++;
 	}
-return (j);
+	return (j);
 }
