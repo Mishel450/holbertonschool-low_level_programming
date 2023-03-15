9afe5be87db33c2b5917cdd00370875daed83bbb
Mishel450 <tomasmishel18@gmail.com>
2023-03-15 10:47:32 -0700

    malloc free

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <string.h>
 /**
- *
- *
- *
+ * *_strdup - check the code.
+ * @*str: is the pointer to copy.
+ * Return: s or 0.
  */
 char *_strdup(char *str)
 {
@@ -13,8 +13,8 @@ char *_strdup(char *str)
 
 	if (s == 0)
 		return (0);
-	if (str == NULL)
-                return (NULL);
+	if (str == 0)
+		return (0);
 	for (i = 0; i < size; i++)
 	{
 		s[i] = str[i];
