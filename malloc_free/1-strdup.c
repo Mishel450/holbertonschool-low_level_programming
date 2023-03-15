59b96248f4996e5c6a47664de016d1eaa378133f
Mishel450 <tomasmishel18@gmail.com>
2023-03-15 11:12:03 -0700

    malloc free

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -7,17 +7,19 @@
  */
 char *_strdup(char *str)
 {
+	if (str != NULL)
+	{
 	int i;
 	int size = strlen(str);
 	char *s = malloc(sizeof(char) * size + 1);
 
 	if (s == NULL)
 		return (NULL);
-	if (str == NULL)
-		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		s[i] = str[i];
 	}
 	return (s);
+	}
+return (NULL);
 }
