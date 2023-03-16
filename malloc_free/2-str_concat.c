388c64b9b1ad8cd9cf3c6181455ac38228fc2790
Mishel450 <tomasmishel18@gmail.com>
2023-03-16 07:01:15 -0700

    malloc free

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *str_concat - check the code.
+ * @s1: the first string to concatenate.
+ * @s2: the second string to concatenate.
+ * Return: s3 or NULL.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int size1 = strlen(s1);
+	int size2 = strlen(s2);
+	int size3 = size1 + size2;
+	int i;
+	int j = 0;
+	char s3 = malloc(sizeof(char) * size3 + 1);
+
+	for (i = 0; i < size3; i++)
+	{
+		if (i <= s1)
+		{
+			s3[i] = s1[i];
+		}
+		if (i > s1)
+		{
+			s3[i] = s2[j];
+			j++;
+		}
+	}
+	return (s3);
+}
