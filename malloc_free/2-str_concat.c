84beb36954fb9e083e404628f81453476fa9addd
Mishel450 <tomasmishel18@gmail.com>
2023-03-16 07:02:59 -0700

    malloc free

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -17,11 +17,11 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; i < size3; i++)
 	{
-		if (i <= s1)
+		if (i <= size1)
 		{
 			s3[i] = s1[i];
 		}
-		if (i > s1)
+		if (i > size1)
 		{
 			s3[i] = s2[j];
 			j++;
