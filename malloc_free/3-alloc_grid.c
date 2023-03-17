17c4e6c2c0e2d4b6b7b74421bea9c382f9bb3ea6
Mishel450 <tomasmishel18@gmail.com>
2023-03-17 11:13:37 -0700

    malloc free

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 		int **s = malloc((int) * height + 1)((int) * width + 1);
 		int i = 0;
 		int j = 0;
-		for ( ; i < heighy; i++)
+		for ( ; i < height; i++)
 		{
 			for ( ; j < width; j++)
 			{
