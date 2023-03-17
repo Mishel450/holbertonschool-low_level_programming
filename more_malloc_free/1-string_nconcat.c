c09966fff3b55516866d51045d23890abd957716
Mishel450 <tomasmishel18@gmail.com>
2023-03-17 12:35:37 -0700

    malloc free

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,61 @@
+#include "main.h"
+#include <string.h>
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i;
+	unsigned int j = 0;
+	unsigned int sizeS1 = strlen(s1);
+	unsigned int sizeS2 = strlen(s2);
+	unsigned int sizeAll = sizeS1 + n;
+	unsigned int sizeS3 = sizeS1 + sizeS2;
+	char sAll = malloc(sizeof(char) * sizeAll + 1);
+	char s3 = malloc(sizeof(char) * sizeS3 + 1);
+
+	if (s3 == NULL || sAll == NULL)
+	{
+		return (NULL);
+	}
+	if (n < sizeS2)
+	{
+		for (i = 0; i < sizeAll; i++)
+	{
+		if (i < size1)
+		{
+			sAll[i] = s1[i];
+		}
+		else
+		{
+			sAll[i] = s2[j];
+			j++;
+		}
+	}
+	j = 0;
+	return (sAll);
+	}
+
+	if (n > sizeS2)
+	{
+	for (i = 0; i < size3; i++)
+	{
+		if (i < size1)
+		{
+			s3[i] = s1[i];
+		}
+		else
+		{
+			s3[i] = s2[j];
+			j++;
+		}
+	}
+	j = 0;
+	return (s3);
+	}
+return (0);
+}
