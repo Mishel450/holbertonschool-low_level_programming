6e7030e7bc86f9bef097c0753c57cbf6cb8f8005
Mishel450 <tomasmishel18@gmail.com>
2023-03-21 04:22:11 -0700

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * struct dog - is the name, age and owner of a dog.
+ * @name: the name of the dog.
+ * @age: the age of the dog.
+ * @owner: the owner of the dog.
+ *
+ * Description: this structure contains the information of the dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
