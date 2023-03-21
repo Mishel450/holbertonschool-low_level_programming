54a9238dfc8cc6b3d0870542b769728d2b9c9767
Mishel450 <tomasmishel18@gmail.com>
2023-03-21 06:04:14 -0700

    dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -8,6 +8,8 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char name1 = malloc((char) * strlen(name) + 1);
-	char owner1 = malloc((char) * strlen(owner) + 1);
+	char sizeName = strlen(name);
+	char sizeOwner = strlen(owner);
+	char name1 = malloc((char) * sizeName + 1);
+	char owner1 = malloc((char) * sizeOwner + 1);
 }
