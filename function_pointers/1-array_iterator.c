fe90e0374c84cc673bfff8d6e0fafb61551dfc18
Mishel450 <tomasmishel18@gmail.com>
2023-03-22 05:41:16 -0700

    function_pointers

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,6 +1,5 @@
 #include "function_pointers.h"
 #include <stdlib.h>
-#include <stddef.h>
 /**
  * array_iterator - see in the arrays. 
  * @array: .
@@ -11,9 +10,11 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 unsigned int i;
-
+if (array != NULL && size > 0)
+{
 for (i = 0; i < size; i++)
 {
-	(action)(array[i]);
+	action(array[i]);
+}
 }
 }
