c004285964fe77671358c75207fe9df8d6196f54
Mishel450 <tomasmishel18@gmail.com>
2023-03-22 06:57:01 -0700

    variadic functions

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+/**
+ *
+ *
+ *
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list ar;
+unsigned int i;
+int Num;
+
+va_start(ar, n);
+
+Num = 0;
+for (i = 0; i < n; i++)
+{
+	Num = va_arg(ar, int);
+	printf("%d", Num);
+	if (separator != NULL && i != n - 1)
+	{
+		printf(separator);
+	}
+}
+va_end(ar);
+}
