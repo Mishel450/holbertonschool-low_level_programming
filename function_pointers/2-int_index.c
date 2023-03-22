ff63266818b29c1bfc76086ff6c3ec916234c5a6
Mishel450 <tomasmishel18@gmail.com>
2023-03-22 06:01:17 -0700

    function_pointers

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,13 +1,15 @@
 #include "function_pointers.h"
 #include <stdlib.h>
 /**
- *
- *
- *
+ * int_index - is the function thats search for a matches.
+ * @array: is the array.
+ * @size: is the size of the array.
+ * @cmp: is the function thats points to int_index.
+ * Return: i or -1.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	if (size > 0)
+	if (size > 0 && array != NULL && cmp != NULL)
 	{
 		int i;
 
