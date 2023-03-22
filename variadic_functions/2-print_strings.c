0ee8041af3777baefa27b2fda5db9cd8ce729386
Mishel450 <tomasmishel18@gmail.com>
2023-03-22 07:22:42 -0700

    variadic functions

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -9,14 +9,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ar;
 	unsigned int i;
-	int Num;
+	char Num;
 
 	va_start(ar, n);
 
 	Num = 0;
 	for (i = 0; i < n; i++)
 	{
-		Num = va_arg(ar, int);
+		Num = va_arg(ar, char);
 		printf("%s", Num);
 		if (separator != NULL && i != n - 1)
 		{
