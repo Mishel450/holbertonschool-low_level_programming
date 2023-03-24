06936a989ee2034328c7a0cc67752801b375582b
Mishel450 <tomasmishel18@gmail.com>
2023-03-24 06:02:53 -0700

    variadic functions

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -11,24 +11,23 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ar;
 	unsigned int i;
-	int check = 0;
-int num;
+	char check = 0;
+char num;
 
 	va_start(ar, n);
 
 	num = 0;
 	for (i = 0; i < n; i++)
 	{
-		check = va_arg(ar, int);
+		check = va_arg(ar, char);
 		if (check == 0)
 		{
 			printf("(nil)");
 		}
 		else
 		{
-			num = va_arg(ar, int);
-			printf("%s", num);
-		}
+			num = va_arg(ar, char);
+
 		if (separator != NULL && i != n - 1)
 		{
 			printf("%s", separator);
