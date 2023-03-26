4a24815c7e9daa980ae47ac16d87c8a6ff1b19a3
Mishel450 <tomasmishel18@gmail.com>
2023-03-26 16:19:07 -0700

    variadic functions

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,40 +2,46 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- *
- *
- *
+ * print_all - check the code.
+ * @format: the const of int, char, float and string.
+ * Return: nothing.
  */
 void print_all(const char * const format, ...)
 {
-va_list ar;
-char numC;
-int numI;
-float numF;
-char *numS;
+	va_list ar;
+	char *string;
+	int i = 0, j, formatS;
 
-va_start(ar, format);
-
-int i = va_arg(ar, int);
-
-	switch(i)
+	if (format)
+		formatS = strlen(format);
+	va_start(ar, format);
+	while (format && format[i])
 	{
-case 'c':
-numC = va_arg(ar, char);
-printf("%d", numC);
-break;
-case 'i':
-numI = va_arg(ar, int);
-printf("%d", numI);
-break;
-case 'f':
-numF = va_arg(ar, float);
-printf("%f", numF);
-break;
-case 's':
-numS = va_arg(ar, char *);
-printf("%s", numS);
-break;
+		j = 0;
+		switch(i)
+		{
+			case 'c':
+				printf("%d", va_arg(ar, int));
+				j = 1;
+				break;
+			case 'i':
+				printf("%d", va_arg(ar, int));
+				j = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(ar, double));
+				j = 1;
+				break;
+			case 's':
+				string = va_arg(x, char *);
+				!string ? printf("(nil)") : printf("%s", string);
+				j = 1;
+				break;
+		}
+		if (cond == 1 && i < formatSelect - 1)
+			printf(", ");
+		i++;
 	}
+	printf("\n");
 	va_end(ar);
 }
