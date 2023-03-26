1076e030bc231b3c9d6e5ca358f1f52ce17a29f7
Mishel450 <tomasmishel18@gmail.com>
2023-03-26 16:00:21 -0700

    variadic functions

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -9,36 +9,31 @@
 void print_all(const char * const format, ...)
 {
 va_list ar;
-char *i;
-
+char numC;
+int numI;
+float numF;
+char *numS;
 
 va_start(ar, format);
 
-while (i < format)
-{
 	switch(ar)
 	{
 case 'c':
-char numC;
 numC = va_arg(ar, char);
 printf("%d", numC);
 break;
 case 'i':
-int numI;
 numI = va_arg(ar, int);
 printf("%d", numI);
 break;
 case 'f':
-float numF;
 numF = va_arg(ar, float);
 printf("%f", numF);
 break;
 case 's':
-char *numS;
 numS = va_arg(ar, char *);
 printf("%s", numS);
 break;
 	}
-i++
-}			va_end(ar);
+	va_end(ar);
 }
