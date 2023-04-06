3ef9e40814dc71ffe71ea6d9e0906dc1628ad9a6
Mishel450 <tomasmishel18@gmail.com>
2023-04-06 16:57:27 -0700

    lists

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - it return the len of the list.
+ * @h: is the list.
+ * Return: i.
+ */
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+	
+	while (h != NULL)
+	{
+		i++;
+	h = h->next;
+	}	
+return (i);
+}
