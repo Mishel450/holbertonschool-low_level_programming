f2bc9b77a4c6746b25ea9455536579c1db0edcb8
Mishel450 <tomasmishel18@gmail.com>
2023-04-08 14:36:33 -0700

    singly linked lists

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ *
+ *
+ *
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *Node;
+	
+	Node = malloc(sizeof(list_t));
+
+	if (Node == NULL)
+		return (NULL);
+	Node->str = strdup(str);
+	Node->len = strlen(str);
+	Node->next = *head;
+	*head = Node;
+	return (Node);
+}
