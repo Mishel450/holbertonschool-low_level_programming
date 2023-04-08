16edd3b6f3ab18f479a50526d202dd2206b3ce7e
Mishel450 <tomasmishel18@gmail.com>
2023-04-08 15:24:30 -0700

    doubly linked lists

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,16 +1,16 @@
 #include "lists.h"
 /**
- *
- *
- *
+ * print_dlistint - prints all the list and the len.
+ * @h: the list.
+ * Return: i (the len of the list).
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-int i = 0;
+	int i = 0;
 
 	while (h != NULL)
 	{
-			printf("%d\n", h->n);
+		printf("%d\n", h->n);
 		i++;
 		h = h->next;
 	}
