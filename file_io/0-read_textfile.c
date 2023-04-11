863242ce52e3d9fd83753ab0080d55ffabbb041b
Mishel450 <tomasmishel18@gmail.com>
2023-04-11 06:49:25 -0700

    file io

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -12,7 +12,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename != NULL)
 	{
 		int filestore = open("filename", O_RDWR);
-		write(1, &filestore, letters);
+		write(2, &filestore, letters);
 		return (letters);
 	}
 	return (0);
