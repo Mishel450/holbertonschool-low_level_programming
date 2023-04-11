9c8fcc9aed47ebed364871e25b9c5a91f0f7edf9
Mishel450 <tomasmishel18@gmail.com>
2023-04-11 05:24:26 -0700

    file io

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text and prints it.
+ * @filename: the text.
+ * @letters: the number of letters of the text.
+ * Return: 0 or letters.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	if (filename == NULL)
+		return (0);
+	if (filename != NULL)
+	{
+		write(1, &filename, letters);
+		return (letters);
+	}
+	return (0);
+}
