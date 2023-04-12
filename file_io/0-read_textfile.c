5994bd7d9ed730912d57eed3374472354c21a04d
Mishel450 <tomasmishel18@gmail.com>
2023-04-12 04:22:14 -0700

    file io

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -8,13 +8,15 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int filechecker;
-
+char filewriter = malloc(sizeof(char) * letters);
+	
 	if (filename == NULL)
 		return (0);
-	filechecker = open(filename, O_RDWR);
+	filechecker = open(filename, O_RDONLY);
 	if (filechecker == -1)
 		return (0);
-	write(1, &filename, letters);
+	read(filename, filewriter, letters);
+	write(1, &filewriter, letters);
 	close(filechecker);
 	return (letters);
 }
