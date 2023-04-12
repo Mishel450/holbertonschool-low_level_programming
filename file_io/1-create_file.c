1c945a8d9c2d77adec83c85d9b096c8fff001922
Mishel450 <tomasmishel18@gmail.com>
2023-04-12 10:01:36 -0700

    file io

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,10 +13,9 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_APPEND | O_CREAT, 0600);
 	if (fd == -1)
 		return (-1);
-	if (*text_content != NULL)
+	if (text_content != NULL)
 	{
-		n = read(fd, text_content, strlen(text_content));
-		read(n, fd, strlen(text_content));
+		write(fd, text_content, strlen(text_content));
 		return (1);
 	}
 	if (text_content == NULL)
