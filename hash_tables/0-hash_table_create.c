b476bdd4f0137614b2ce80330f4b5936f5a14b51
Mishel450 <tomasmishel18@gmail.com>
2023-04-12 12:28:07 -0700

    hash tables

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -6,8 +6,9 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht[size];
+	hash_table_t ht;
 
+	ht = malloc(sizeof(size));
 	if (ht == NULL)
 		return (NULL);
 	return (&ht);
