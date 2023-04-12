574e83a1a7f8f8c4fdaef6c91dcf51e59c4d5e32
Mishel450 <tomasmishel18@gmail.com>
2023-04-12 10:07:46 -0700

    file io

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- *
- *
- *
+ * create_file - it creates a file with a name and content (or not).
+ * @filename: is the name of the file.
+ * @text_content: is the content of the file (it can be empty).
+ * Return: -1 (in error) or 1.
  */
 int create_file(const char *filename, char *text_content)
 {
