42a94d778f10093adb033dd49d5ff5d48d136ae8
Mishel450 <tomasmishel18@gmail.com>
2023-04-12 12:24:37 -0700

    hash tables

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,12 +1,12 @@
 #include "hash_tables.h"
 /**
- * hash_table_t - makes a hash table with the len of size.
+ * hash_table_create - makes a hash table with the len of size.
  * @size: is the len of the array.
  * Return: ubication of array or NULL (in case of error).
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int ht[size];
+	hash_table_t ht[size];
 
 	if (ht == NULL)
 		return (NULL);
