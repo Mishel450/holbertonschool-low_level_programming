519ca688a0e757cb02abdb4cb6bc08e8f68363fb
Mishel450 <tomasmishel18@gmail.com>
2023-04-12 06:22:50 -0700

    file io

---
+++ file_io/1-create_file.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_RDWR | O_CREAT, 500);
+	return (1);
+
+}
