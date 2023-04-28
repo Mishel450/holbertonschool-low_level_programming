f409bf75d6ecabfb96e7f49e51b00f3e6ad6bf41
Mishel450 <tomasmishel18@gmail.com>
2023-04-28 13:30:04 -0700

    pointers_arrays_strings

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -11,15 +11,18 @@ void print_diagsums(int *a, int size)
 	int i;
 	int j = 0;
 	int h = 0;
+int plus = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		j += *(a(i)(i));
+		plus = a[i][i];
+		j += plus;
 	}
 	i = 0;
 	for (i = size; i > 0; i--)
 	{
-		h += *(a(i)(i));
+		plus = a[i][i];
+		h += plus;
 	}
 	printf("%d, %d", j, h);
 }
